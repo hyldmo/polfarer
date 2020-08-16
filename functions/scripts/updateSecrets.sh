87bf9e6e86a15c8a24b8cf17bd6d817b51637bc3
hyldmo <eivind_h@outlook.com>
2020-08-16 21:40:50 +0200

    Fixed secret uploading

--- functions/scripts/updateSecrets.sh
+++ functions/scripts/updateSecrets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-firebase functions:config:set --token="$FIREBASE_TOKEN" \
+yarn firebase functions:config:set --token="$FIREBASE_TOKEN" \
 	polfarer.pg_host="$pg_host" \
 	polfarer.pg_database="$pg_database" \
 	polfarer.pg_user="$pg_user" \
