1680e3186abb2663ceb8690d55e10f0420212940
hyldmo <eivind_h@outlook.com>
2021-05-23 19:34:05 +0200

    Updated secrets script

--- functions/scripts/updateSecrets.sh
+++ functions/scripts/updateSecrets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yarn firebase functions:config:set --token="$FIREBASE_TOKEN" \
-	polfarer.pg_host="$pg_host" \
-	polfarer.pg_database="$pg_database" \
-	polfarer.pg_user="$pg_user" \
-	polfarer.pg_password="$pg_password"
+	polfarer.db_host="$db_host" \
+	polfarer.db_database="$db_database" \
+	polfarer.db_user="$db_user" \
+	polfarer.db_password="$db_password"
